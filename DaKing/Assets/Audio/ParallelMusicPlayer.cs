707e34cc50216a5032c8e1a283837e1d27ae462d
DrManticore <doktormantikore@hotmail.com>
2016-09-06 22:01:24 +0100

    Added initial structure of ParallelMusicPlayer class

---
+++ DaKing/Assets/Audio/ParallelMusicPlayer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public struct PlayerAudioData {
+	public int fadeDir;
+	public float transitionDuration;
+	public float fadeTimer;
+}
+
+public class ParallelMusicPlayer : MonoBehaviour {
+
+	public List<AudioClip> sounds;
+	public List<AudioSource> sources;
+
+	public PlayerAudioData[] additionalAudioData;
+
+	public float defaultTransitionTime = 1f;
+	// Use this for initialization
+	void Start () {
+	
+		sources = new List<AudioSource> ();
+
+		additionalAudioData = new PlayerAudioData[sounds.Count];
+
+		for (int i = 0; i < sounds.Count; i++) {
+			AudioSource theSource = transform.gameObject.AddComponent<AudioSource>();
+			sources.Add (theSource);
+			sources[i].clip = sounds[i];
+			sources[i].volume = 0;
+			additionalAudioData [i] = new PlayerAudioData ();
+			sources [i].Play ();
+		}
+
+	}
+
+}
