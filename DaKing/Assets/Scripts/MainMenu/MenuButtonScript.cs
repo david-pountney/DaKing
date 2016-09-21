261f9456afccb58edd256442eae6716b2b61b065
subwayheaven <subwayheaven@gmail.com>
2016-09-21 19:26:09 +0100

    Random temp sound effects

---
+++ DaKing/Assets/Scripts/MainMenu/MenuButtonScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuButtonScript : MonoBehaviour {
+
+    public AudioClip audioClip;
+
+    private AudioSource audioSource;
+
+    // Use this for initialization
+    void Awake () {
+        audioSource = Camera.main.GetComponent<AudioSource>();    
+	}
+
+    public void OnSelect()
+    {
+        audioSource.PlayOneShot(audioClip);
+    }
+}
