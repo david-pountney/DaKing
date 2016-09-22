ce0cda9943da034362dab6a91ebc6930b3527eb0
DrManticore <doktormantikore@hotmail.com>
2016-09-22 22:33:40 +0100

    Created child class of ParallelMusicPlayer to handle the actual mood based transition logic

---
+++ DaKing/Assets/Audio/KDMoodMusicPlayer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KDMoodMusicPlayer : ParallelMusicPlayer {
+
+	public int currentMoodIndex;
+
+	public int getNewMoodIndex(float mood) {
+		float newMood = mood / 100f;
+		if (newMood >= 0.5f) {
+			return 0;
+		} else if (newMood < 0.5f && newMood > 0.25f) {
+			return 1;
+		} else {
+			return 2;
+		}
+	}
+
+	public void transitionMood(float mood) {
+		int newMoodIndex = getNewMoodIndex (mood);
+		base.fadeOutTrack (currentMoodIndex);
+		base.fadeInTrack (newMoodIndex);
+		currentMoodIndex = newMoodIndex;
+	}
+
+	public void setMoodIndex(float mood) {
+		currentMoodIndex = getNewMoodIndex (mood);
+	}
+
+}
