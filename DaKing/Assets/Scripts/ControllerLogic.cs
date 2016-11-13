1ce5991df826642583f156b635ab7b1b42b95e02
subwayheaven <subwayheaven@gmail.com>
2016-11-13 01:23:05 +0000

    Added final artwork from Lilo to the project and scene Rearranged objects to fit a bit better

--- DaKing/Assets/Scripts/ControllerLogic.cs
+++ DaKing/Assets/Scripts/ControllerLogic.cs
@@ -52,9 +52,7 @@ public class ControllerLogic : MonoBehaviour {
             i++;
         }
     }
-
-
-
+    
     public void Init()
     { 
         musicController.GetComponent<SimpleMusicController>().fade_in();
