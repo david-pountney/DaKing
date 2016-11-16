ab653ffac24cd2935cb7fa7fa161a5ac1c20127d
DrManticore <doktormantikore@hotmail.com>
2016-11-16 21:51:51 +0000

    Added functionality for a -1 index in the mood music player as a placeholder for a fourth song

--- DaKing/Assets/Audio/KDMoodMusicPlayer.cs
+++ DaKing/Assets/Audio/KDMoodMusicPlayer.cs
@@ -7,19 +7,27 @@ public class KDMoodMusicPlayer : ParallelMusicPlayer {
 
 	public int getNewMoodIndex(float mood) {
 		float newMood = mood / 100f;
-		if (newMood >= 0.5f) {
+		if (newMood >= 0.75f) {
 			return 0;
-		} else if (newMood < 0.5f && newMood > 0.25f) {
+		} else if (newMood < 0.75f && newMood > 0.5f) {
 			return 1;
-		} else {
+		} else if (newMood < 0.5f && newMood > 0.25f) {
 			return 2;
+		} else {
+			return -1;
 		}
 	}
 
 	public void transitionMood(float mood) {
 		int newMoodIndex = getNewMoodIndex (mood);
-		base.fadeOutTrack (currentMoodIndex);
-		base.fadeInTrack (newMoodIndex);
+		if (currentMoodIndex > -1) {
+			base.fadeOutTrack (currentMoodIndex);
+		}
+		if (newMoodIndex > -1) {
+			base.fadeInTrack (newMoodIndex);
+		} else {
+			base.fadeOutAll ();
+		}
 		currentMoodIndex = newMoodIndex;
 	}
 
