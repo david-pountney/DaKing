536380a0618d6c2b1bff26072fba50b80a5bae4d
subwayheaven <subwayheaven@gmail.com>
2017-05-05 21:33:49 +0100

    Edited soundDef slightly Changed serventText file Added example sound

---
+++ DaKing/Assets/Scripts/CharacterSpeechAudioScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CharacterSpeechAudioScript : MonoBehaviour {
+
+    public List<GameObject> listOfCharacterSpeechAudio;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
